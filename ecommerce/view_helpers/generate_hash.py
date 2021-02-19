2ea72e4a251f1353872b5b747267792cb856cab2
Lasha Sherozia <repeop0@gmail.com>
2021-02-20 01:29:47 +0400

    view_helpers/generate_hash.py/generation function now checks whether generated hash_code is unique.

--- ecommerce/view_helpers/generate_hash.py
+++ ecommerce/view_helpers/generate_hash.py
@@ -1,10 +1,24 @@
 from random import randint
 
+from ecommerce.models import Ticket
+
 
 def generation() -> str:
     """Generates 50 characters length hash_code to be used for a ticket."""
-    hash_code = ''
-    for i in range(50):
-        hash_code += chr(randint(48, 122))
+    while True:
+        hash_code = ''
+        for i in range(50):
+            hash_code += chr(randint(48, 122))
+
+        tickets = Ticket.objects.all()
+        to_continue = False
+
+        for ticket in tickets:
+            if ticket.hash_code == hash_code:
+                to_continue = True
+        if to_continue:
+            continue
+
+        break
 
     return hash_code
